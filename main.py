9f54974645737be21c593b5cdbf2dce88e08c98c
Alejandro Almira <aalmiramolla@gmail.com>
2017-10-22 22:31:55 +0200

    Added requeriments

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-##TODO: Improve the next two lines
-import sys
-sys.path.append("Bot/")
+# TODO: Improve the next two lines
+# import sys
+# sys.path.append("Bot/")
 
 from Bot import BUABot
 
-##TODO: Change the content of these variables by yours BUA credials. Only for testing. Remember to erase it when you push a change in the repo.
+# TODO: Change the content of these variables by yours BUA credials. Only
+# for testing. Remember to erase it when you push a change in the repo.
 username = '0XXXXXXXX'
 secret = 'YYYY'
 
 
 def main():
     buaBot = BUABot()
-    buaBot.login(username, secret) 
+    buaBot.login(username, secret)
     buaBot.showLoans()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()

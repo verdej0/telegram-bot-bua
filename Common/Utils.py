ad6087f32a16a210b22172365c7dfe1bc2c62097
Verdejo <verdejod97@gmail.com>
2017-10-25 23:28:05 +0200

    Update Utils.py

--- Common/Utils.py
+++ Common/Utils.py
@@ -3,4 +3,4 @@
 
 
 catalogBookRegex = r"<label class=\"itemlisting.*\" for=\"(.*)\">\\n<!-- title -->\\n<strong>(.*)<\/strong>\\n<!-- and\/or display linked 880 data -->\\n<!-- and\/or display linked 880 data -->\\n<!-- author -->\\n<br\/>(.*)<!-- and\/or display linked 880 data -->\\n<\/label>"
-loanBooksRegex =  r"<td class=\"itemlisting.*?\">\s+<input type=\"checkbox\" name=\".*?\" id=\"RENEW.*?\">\s+<\/td>\s+<td class=\"itemlisting.*?\">\s+<label for=\"RENEW.*?\">\s+<!-- Print the title, if it exists -->\s+(.*?)\s+<!-- Print the author, if it exists -->\s+(.*?)\s+<\/label>\s+<\/td>\s+<td class=\"itemlisting.*?\">\s+devolución:\s+<!.. Print the date due -->\s+<strong>(.*?)<\/strong><br>"
+loanBooksRegex =  r"<td class=\"itemlisting.*\">\s*<input type=\"checkbox\" name=\".*\" id=\"RENEW.*\">\s*<\/td>\s*<td class=\"itemlisting.*\">\s*<label for=\"RENEW.*\">\s*<!-- Print the title, if it exists -->\s*(.*)&nbsp;&nbsp;\s*<!-- Print the author, if it exists -->\s*(.*)\s*<\/label>\s*<\/td>\s*<td class=\"itemlisting.*\" align=\"left\">\s*devoluci.*n:\s*<!-- Print the date due -->\s*<strong>(.*)<\/strong><br>\s*<\/td>"
